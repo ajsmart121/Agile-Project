990bbb267bfa72e5ff846a57cf162f5aa1f695d4
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-27 13:29:41 +0000

    Add files via upload

--- Software_Code/userlevellogin.php
+++ Software_Code/userlevellogin.php
@@ -20,10 +20,10 @@ $conn->close();
 <html>
 <body>
 <form action="/action_page.php">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username" value="Liam"><br>
+  <label for="lname">Password:</label><br>
+  <input type="text" id="password" name="password" value="McKenzie"><br><br>
   <input type="submit" value="Submit">
 </form> 
 </body>
