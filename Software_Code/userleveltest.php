e774e40d229f2c444fe0f4f84e7cde66585c2e01
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-27 12:28:54 +0000

    Update userleveltest.php

--- Software_Code/userleveltest.php
+++ Software_Code/userleveltest.php
@@ -1,3 +1,28 @@
 <?php
-echo "Hello";
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+// Create connection
+$conn = new mysqli($server, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT ID, Forename, Surname, Email, UserLevelID FROM user";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["PersonID"]. " - Name: " . $row["Forename"]. " " . $row["Surname"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
 ?>
