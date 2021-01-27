de49b5dc40d679d7d1dbe1b775cebb5dadc3ccdb
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-27 12:33:20 +0000

    Update userleveltest.php

--- Software_Code/userleveltest.php
+++ Software_Code/userleveltest.php
@@ -19,7 +19,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["ID"]. " - Name: " . $row["Forename"]. " " . $row["Surname"]. "<br>";
+    echo "User ID: " . $row["ID"]. " - Name: " . $row["Forename"]. " " . $row["Surname"]. "<br>";
   }
 } else {
   echo "0 results";
