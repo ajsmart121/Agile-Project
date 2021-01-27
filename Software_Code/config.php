ef5ce24593b5f69e3729cac57d9360d24d561e48
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-27 14:42:05 +0000

    Add files via upload

--- Software_Code/config.php
+++ Software_Code/config.php
@@ -6,10 +6,12 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-// Create connection
-$conn = new mysqli($server, $username, $password, $db);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+try {
+    $conn = new PDO("mysql:host=".$server.";dbname=".$db, $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+  } catch(PDOException $e) {
+    //echo "Connection failed: " . $e->getMessage();
+  }
 ?>
--- Software_Code/userlevellogin.php
+++ Software_Code/userlevellogin.php
@@ -3,18 +3,18 @@
 session_start();
 include"config.php";
 
-$sql = "SELECT ID, Forename, Surname, Email, UserLevelID FROM user";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo "User ID: " . $row["ID"]. " - Name: " . $row["Forename"]. " " . $row["Surname"]. "<br>";
-  }
-} else {
-  echo "0 results";
+try{
+	$userFind = $conn->prepare("SELECT * FROM user
+	WHERE Username = '$username' AND Password = '$password'");
+	$userFind->execute();
+	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
 }
-$conn->close();
+
+catch(PDOException $e){
+	echo $userFind . "<br>" . $e->getMessage();
+}
+
+$conn = null;
 ?>
 
 
@@ -22,7 +22,7 @@ $conn->close();
 <body>
 <form action="userleveltest.php" method="post">
   <label for="username">Username:</label><br>
-  <input type="text" id="username" name="username" value="LiamMcKenzie"><br>
+  <input type="text" id="username" name="username" value="Liam"><br>
   <label for="lname">Password:</label><br>
   <input type="text" id="password" name="password" value="Password"><br><br>
   <input type="submit" value="Submit">
