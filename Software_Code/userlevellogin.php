330bc1b9de7cce45d5995c5f97a78b090466af7a
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-27 15:17:10 +0000

    Add files via upload

--- Software_Code/userlevellogin.php
+++ Software_Code/userlevellogin.php
@@ -9,9 +9,11 @@ $conn = null;
 <html>
 <body>
 <form action="userleveltest.php" method="post">
+  <label for="study">Study ID:</label><br>
+  <input type="text" id="study" name="study" value="1"><br>
   <label for="username">Username:</label><br>
   <input type="text" id="username" name="username" value="Liam"><br>
-  <label for="lname">Password:</label><br>
+  <label for="password">Password:</label><br>
   <input type="text" id="password" name="password" value="Password"><br><br>
   <input type="submit" value="Submit">
 </form> 
--- Software_Code/userleveltest.php
+++ Software_Code/userleveltest.php
@@ -3,13 +3,14 @@
 session_start();
 include"config.php";
 
+$study = $_POST["study"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
 
 try{
-	$userFind = $conn->prepare("SELECT * FROM user
-	WHERE Username = '$username' AND Password = '$password'");
+	$userFind = $conn->prepare("SELECT * FROM ul.user
+	WHERE ul.Username = '$username' AND ul.Password = '$password'");
 	$userFind->execute();
 	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
 	
