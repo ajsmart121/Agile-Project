6af24876962df5b5fcecc9a63d2df75c1ada8e41
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-28 14:54:08 +0000

    Add files via upload

--- Software_Code/Liam/questioncreation.php
+++ Software_Code/Liam/questioncreation.php
@@ -6,6 +6,18 @@ $studycreator = $_POST["studycreator"];
 $studyname = $_POST["studyname"];
 $questionquantity = $_POST["questionquantity"];
 
+try{
+	$userFind = $conn->prepare("SELECT * FROM user");
+	$userFind->execute();
+	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
 	
+	echo $userFindResult->Username;
+	echo $userFindResult->Password;
+}
+
+catch(PDOException $e){
+	echo $userFind . "<br>" . $e->getMessage();
+}
+
 $conn = null;
 ?>
