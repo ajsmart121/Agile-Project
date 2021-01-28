f48d5e8798ec439359478019f8371f644523fa0b
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-28 14:52:04 +0000

    Add files via upload

--- Software_Code/Liam/questioncreation.php
+++ Software_Code/Liam/questioncreation.php
@@ -2,6 +2,9 @@
 session_start();
 include"config.php";
 
+$studycreator = $_POST["studycreator"];
+$studyname = $_POST["studyname"];
+$questionquantity = $_POST["questionquantity"];
 
 	
 $conn = null;
