d4a906914d1a082ca83fe644f5c7776208a36054
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-28 14:25:11 +0000

    Add files via upload

--- Software_Code/Liam/questioncreation.php
+++ Software_Code/Liam/questioncreation.php
@@ -7,5 +7,18 @@ $studycreator = $_POST["studycreator"];
 $studyname = $_POST["studyname"];
 $questionquantity = $_POST["questionquantity"];
 
+
+
+
+try{
+	$studyInsert = "INSERT INTO Study (StudyName)
+	VALUES ('$studyname')";
+	$conn->exec($studyInsert);
+}
+
+catch(PDOException $e){
+	echo $studyInsert . "<br>" . $e->getMessage();
+	}
+}
 $conn = null;
 ?>
