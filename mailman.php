f72a497b0ab52db67b91cf07b0b3ae4981042ff8
AstroFlyer <43142147+AstroFlyer@users.noreply.github.com>
2021-01-28 16:41:09 +0000

    Add files via upload

---
+++ mailman.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+
+          Question: <?php echo $_POST["question"];?><br>
+
+          Answer Type: <?php echo $_POST["answer_type"];?><br>
+
+          Required: <?php echo $_POST["required"];?><br>
+          <br>
+
+
+
+</body>
+</html
