a3d6408ebfcc9c485c69ca8d26b8dd5bacbb97c5
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-28 14:55:32 +0000

    Add files via upload

---
+++ Software_Code/Liam/config.php
@@ -0,0 +1,18 @@
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+try {
+    $conn = new PDO("mysql:host=".$server.";dbname=".$db, $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connected successfully";
+}
+catch(PDOException $e){
+	echo "Connection failed: " . $e->getMessage();
+}
+?>
