67ae27b7759f7df23b0cae96bf3f41ac719f12b6
AstroFlyer <43142147+AstroFlyer@users.noreply.github.com>
2021-01-28 11:48:54 +0000

    Add files via upload

---
+++ Software_Code/index.php
@@ -0,0 +1,4 @@
+<html>
+<body><?php echo "hello world"; ?>
+</body>
+</html
---
+++ Software_Code/mailman.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+
+          Question <?php echo $_POST["count"]; ?><br>
+                    <?php echo $_POST["questions[]"];?>
+        
+
+</body>
+</html
