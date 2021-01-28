b77c3400e43864d4395be59fb96386e6eeb46d24
AstroFlyer <43142147+AstroFlyer@users.noreply.github.com>
2021-01-28 13:19:52 +0000

    Add files via upload

--- Software_Code/Stewart/mailman.php
+++ Software_Code/Stewart/mailman.php
@@ -2,9 +2,8 @@
 <body>
 
 
-          Question <?php echo $_POST["count"]; ?><br>
-                    <?php echo $_POST["questions[]"];?>
-        
+          Question <?php echo $_POST["questions[]"];?>
+
 
 </body>
 </html
