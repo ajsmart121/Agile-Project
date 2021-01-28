e7827091fe0e5436fab56a5d99a93cec772306d6
AstroFlyer <43142147+AstroFlyer@users.noreply.github.com>
2021-01-28 16:42:52 +0000

    Add files via upload

--- phpinputtest.php
+++ phpinputtest.php
@@ -1,37 +1,3 @@
-<html>
-<body>
-      <form id="quiz" action="/mailman.php" method="POST">
-  <p>Question 1</p>
-          <input type="text" name="question"/>
-
-          <p> Answer Type: </p>
-          <select id="answer_type" name="answer_type">
-            <option value="radio Button">Radio Button</option>
-            <option value="checkbox">Checkbox</option>
-            <option value="textbox">text box</option>
-            <option value="drop down">drop down</option>
-          </select><br>
-          <br>
-          <label for="required">Required question?</label><br>
-          <input type="radio" name="required">
-          <br>
-
-          ==========================================================
-
-          <input type="submit" value="Submit the Questionaire"/>
-
-          </form>
-</body>
-</html>
-
-
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html>
         <head>
