e1b3726e6679d27c786a9aa5ecfd0355edfdd625
AstroFlyer <43142147+AstroFlyer@users.noreply.github.com>
2021-01-29 13:37:43 +0000

    Add files via upload

--- mailman.php
+++ mailman.php
@@ -4,16 +4,18 @@
         $count = 1;
 
       echo "Questionnaire: " . $_POST['name'];
-      echo ($_POST["question"] . $count);
+
 
               while($count <= 10)
               {
 
-                    if(strlen($_POST["required"] . $count) == 0){
-                        break;}
+                  //  if(strlen($_POST["required"] . $count) == 0){
+                    //    break;}
 
 
                     echo "Question " . $count .": ". $_POST["question" . ($count)] . ", ";
+
+                    echo strlen($_POST["question"] . $count);
 
                     echo  "Answer Type: " . $_POST["answer_type" . ($count)] . ", ";
 
