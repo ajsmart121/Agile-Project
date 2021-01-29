4c899dbcefaede6f43e8d984ae3d50f2d2c237dd
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-29 05:17:28 +0000

    Add files via upload

--- questioncreation.php
+++ questioncreation.php
@@ -2,19 +2,48 @@
 session_start();
 include"config.php";
 
-$studycreator = $_POST["studycreator"];
-$studyname = $_POST["studyname"];
 $questionquantity = $_POST["questionquantity"];
 
-try{
-	$userFind = $conn->prepare("SELECT * FROM study");
-	$userFind->execute();
-	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
+if(isset($_POST["studycreator"])){
+	$studycreator = $_POST["studycreator"];
+	$studyname = $_POST["studyname"];
+
+
+	try{
+		$studyInsert = "INSERT INTO Study (UserID, StudyQuestionCount, StudyName)
+		VALUES ('$studycreator', '$questionquantity', '$studyname')";
+		$conn->exec($studyInsert);
+	}
+
+	catch(PDOException $e){
+		echo $studyInsert . "<br>" . $e->getMessage();
+	}
 }
 
-catch(PDOException $e){
-	echo $userFind . "<br>" . $e->getMessage();
-}
+echo $questionquantity;
+$questionquantity = $questionquantity - 1;
+if($questionquantity >= 0){
 	
+?>
+
+
+
+
+<html>
+<body>
+<form action method="post">
+
+	<label for="questiontext">Study Creator ID:</label><br>
+	<input type="text" id="questiontext" name="questiontext" value="Is this an example question?"><br>
+	<input type="hidden" id ="questionquantity" name="questionquantity" value=<?php echo $questionquantity; ?> readonly>
+	
+	<input type="submit" value="Submit">
+	</form> 
+</body>
+</html>
+
+<?php
+}
+
 $conn = null;
 ?>
