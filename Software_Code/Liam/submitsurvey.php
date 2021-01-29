657a4964e05e4280507a8e1e3e00430e345ec3ca
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-29 11:55:41 +0000

    Add files via upload

---
+++ Software_Code/Liam/submitsurvey.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<?php
+session_start();
+include"config.php";
+
+echo $_POST['question1'];
+?>
+
+
+
+<?php
+$conn = null;
+?>
--- Software_Code/Liam/survey.php
+++ Software_Code/Liam/survey.php
@@ -13,18 +13,32 @@ try{
 	WHERE StudyID = '$survey'");
 	$QuestionsFind->execute();
 	$QuestionsFindResult = $QuestionsFind->fetchALL();
-	
-	}
-	catch(PDOException $e){
-		echo "Error: " . $e->getMessage();
-	}
 	$questionCount = count($QuestionsFindResult);
 	
-	for($i = 0; $i < $questionCount; $i++){
-		echo $QuestionsFindResult[$i][0];
-	}
+}
+catch(PDOException $e){
+	echo "Error: " . $e->getMessage();
+}
 
 ?>
+<html>
+<body>
+
+<form action="submitsurvey.php" method="post">
+	<?php
+	for($i = 0; $i < $questionCount; $i++){
+		?>
+		<label for="question <?php $i+1 ?> "> <?php echo $QuestionsFindResult[$i][0]; ?> </label><br>
+		<input type="text" id="question <?php $i+1 ?> " name="question <?php $i+1 ?> " value=""><br>
+	<?php
+	}
+	?>
+	<input type="submit" value="Submit">
+</form> 
+
+</body>
+</html>
+
 
 <?php
 $conn = null;
