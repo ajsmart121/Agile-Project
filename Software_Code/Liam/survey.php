b8ba8730d600149ef57ffa9bd68a9ec98c93b881
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-01-29 09:47:17 +0000

    Add files via upload

--- Software_Code/Liam/survey.php
+++ Software_Code/Liam/survey.php
@@ -7,6 +7,24 @@ include"config.php";
 <?php
 $survey = $_GET['surveyid'];
 echo $survey;
+
+
+try{
+	$QuestionsFind = $conn->prepare("SELECT * FROM Question
+	WHERE StudyID = '$survey'");
+	$QuestionsFind->execute();
+	$QuestionsFindResult = $QuestionsFind->fetch(PDO::FETCH_All);
+	
+	foreach($QuestionsFindResult as $row) {
+		$studyList = $studyList.$row['QuestionText']."\n";
+	}
+}
+catch(PDOException $e){
+	echo "Error: " . $e->getMessage();
+}	
+
+
+
 ?>
 
 <?php
