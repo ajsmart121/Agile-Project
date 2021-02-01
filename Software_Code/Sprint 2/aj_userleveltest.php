4469cccc7f9e2949bd09b861046e4a0d13eabd16
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-01 15:14:38 +0000

    aaaaaaaaaaaaaaaaaaaaaaa2

--- Software_Code/Sprint 2/aj_userleveltest.php
+++ Software_Code/Sprint 2/aj_userleveltest.php
@@ -6,13 +6,12 @@ include"config.php";
 $study = $_POST["study"];
 $username = $_POST["username"];
 $password = password_hash($password, "sha256");
-$hashedPassword = $_POST[$hashedPassword];
-echo $hashedPassword;
+$password = $_POST[$password];
 
 
 try{
 	$userFind = $conn->prepare("SELECT * FROM user
-	WHERE Username = '$username' AND Password = '$hashedPassword'");
+	WHERE Username = '$username' AND Password = '$password'");
 	$userFind->execute();
 	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
 
