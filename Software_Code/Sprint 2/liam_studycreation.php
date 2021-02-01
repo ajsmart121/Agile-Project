082f09bea3da22c591fa44dc233e3e13c6891226
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-01 11:31:11 +0000

    Update liam_studycreation.php

--- Software_Code/Sprint 2/liam_studycreation.php
+++ Software_Code/Sprint 2/liam_studycreation.php
@@ -7,7 +7,7 @@ include"config.php";
 
 <html>
 <body>
-<form action="questioncreation.php" method="post">
+<form action="liam_questioncreation.php" method="post">
   
   <label for="studycreator">Study Creator ID:</label><br>
   <input type="text" id="studycreator" name="studycreator" value="11"><br>
