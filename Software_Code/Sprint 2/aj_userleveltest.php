122d30a5a355f3f22cd6ad9d24dfe28cbd63ce97
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-01 14:28:59 +0000

    testing php things the sequel

---
+++ Software_Code/Sprint 2/aj_userleveltest.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<?php
+session_start();
+include"config.php";
+
+$study = $_POST["study"];
+$username = $_POST["username"];
+$password = $_POST[hash(sha256,"password")];
+
+
+try{
+	$userFind = $conn->prepare("SELECT * FROM user
+	WHERE Username = '$username' AND Password = '$password'");
+	$userFind->execute();
+	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
+
+	echo $userFindResult->Username;
+	echo $userFindResult->Password;
+}
+
+catch(PDOException $e){
+	echo $userFind . "<br>" . $e->getMessage();
+}
+?>
+
+
+<html>
+<body>
+
+</body>
+</html>
+
+<?php
+$conn = null;
+?>
