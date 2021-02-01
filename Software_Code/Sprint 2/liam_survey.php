ae27edebe9a899e81c4a30867e5bb5d7717f16ba
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-01 16:20:07 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_survey.php
+++ Software_Code/Sprint 2/liam_survey.php
@@ -33,10 +33,10 @@ $dataString = serialize($QuestionsFindResult);
 		?>
 		<label for="answer[<?php $i+1 ?>]"> <?php echo $QuestionsFindResult[$i][0]; ?> </label><br>
 		<input type="text" id="answer[<?php $i+1 ?>]" name="answer[<?php $i+1 ?>]" value=""><br>
-		<input type="hidden" name="questionIDs" value=<?php echo $dataString; ?> readonly>
 	<?php
 	}
 	?>
+	<input type="hidden" name="questionIDs" value=<?php echo $dataString; ?> readonly>
 	<input type="submit" value="Submit">
 </form> 
 
