d366941e410f9b4787de9279c5bddb5a73645cf8
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-01 16:47:35 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_submitsurvey.php
+++ Software_Code/Sprint 2/liam_submitsurvey.php
@@ -7,7 +7,7 @@ $userID = $_SESSION['userID'];
 $questions = $_SESSION['questions'];
 
 //print_r($questions);
-echo $questions[1]->['ID'];
+echo $questions[1]->ID;
 
 $userAnswerCount = count($_POST['answer']);
 
