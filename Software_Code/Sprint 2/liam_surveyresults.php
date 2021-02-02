e555b874b3aeca2711462cdf27c649b2b5cce821
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-02 11:42:06 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_surveyresults.php
+++ Software_Code/Sprint 2/liam_surveyresults.php
@@ -2,17 +2,27 @@
 <?php
 session_start();
 include"config.php";
+
+if(isset($_GET["surveyID"])){
+	$surveyID = $_GET["surveyID"];
+}
+if(isset($_GET["userID"])){
+	$userID = $_GET["userID"];
+}
+
 ?>
 
 <html>
 <body>
 
-<form action method="post">
+<form action method="get">
 	This form will allow you to see a particular user's response to a particular survey.
-	<label for="surveyID">Question Text:</label><br>
-	<input type="text" id="surveyID" name="surveyID" value="Please enter the ID of the survey:"><br>
-	<label for="surveyUser">Question Text:</label><br>
-	<input type="text" id="surveyUser" name="surveyUser" value="IPlease enter the ID of the user:"><br>
+	<br>
+	<br>
+	<label for="surveyID">Please enter the ID of the survey:</label><br>
+	<input type="text" id="surveyID" name="surveyID" value=""><br>
+	<label for="surveyUser">Please enter the ID of the user::</label><br>
+	<input type="text" id="userID" name="userID" value=""><br>
 	<input type="submit" value="Submit">
 </form> 
 
