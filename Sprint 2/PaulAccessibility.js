e957350554d0d7ea3f3f4a5a016b3f5d774762d1
PaulCrockford <43142223+PaulCrockford@users.noreply.github.com>
2021-02-02 16:17:52 +0000

    Background colour change working, button colours are not

---
+++ Sprint 2/PaulAccessibility.js
@@ -0,0 +1,35 @@
+function ChangeColorNormal()
+    {
+      document.body.style.backgroundColor = "#3e5bc7";
+      document.getElementById("button2").style.backgroundColor = "#3e5bc7";
+      document.getElementById("button5").style.backgroundColor = "#c42986";
+      document.getElementById("button3").style.backgroundColor = "3e5bc7";
+      document.getElementById("box1").style.borderColor = "#800080";
+    }
+
+    function ChangeColorProtan()
+    {
+      document.body.style.backgroundColor = "#0064b1";
+      document.getElementById("button2").style.backgroundColor = "#0064b1";
+      document.getElementById("button5").style.backgroundColor = "#004186";
+      document.getElementById("button3").style.backgroundColor = "#0064b1";
+      document.getElementById("box1").style.borderColor = "#25486e";
+    }
+
+    function ChangeColorDeutran()
+    {
+      document.body.style.backgroundColor = "#0064b1";
+      document.getElementById("button2").style.backgroundColor = "#0064b1";
+      document.getElementById("button5").style.backgroundColor = "#555e6c";
+      document.getElementById("button3").style.backgroundColor = "#0064b1";
+      document.getElementById("box1").style.borderColor = "#235485";
+    }
+
+    function ChangeColorTritan()
+    {
+      document.body.style.backgroundColor = "#007385";
+      document.getElementById("button2").style.backgroundColor = "#007385";
+      document.getElementById("button5").style.backgroundColor = "#ab3c4b";
+      document.getElementById("button3").style.backgroundColor = "#007385";
+      document.getElementById("box1").style.borderColor = "#7a474c";
+    }
