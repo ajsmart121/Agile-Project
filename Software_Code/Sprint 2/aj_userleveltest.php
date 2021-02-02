d45f6df909bc84053036c0a7d1cebd56d9c12998
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-02 09:13:53 +0000

    Update aj_userleveltest.php
    Edited this file by mistake, reverted to AJ's, will put my edits in liam_*.

--- Software_Code/Sprint 2/aj_userleveltest.php
+++ Software_Code/Sprint 2/aj_userleveltest.php
@@ -1,17 +1,14 @@
-<!DOCTYPE html>
-<?php
-session_start();
-include"config.php";
-
 $study = $_POST["study"];
 $username = $_POST["username"];
 $password = password_hash($password, "sha256");
-//$password = $_POST[$password];
-
+$hashedPassword = $_POST[$hashedPassword];
+echo $hashedPassword;
+$password = $_POST[$password];
 
 
 try{
 	$userFind = $conn->prepare("SELECT * FROM user
+	WHERE Username = '$username' AND Password = '$hashedPassword'");
 	WHERE Username = '$username' AND Password = '$password'");
 	$userFind->execute();
 	$userFindResult = $userFind->fetch(PDO::FETCH_OBJ);
@@ -19,19 +16,14 @@ try{
 	echo $userFindResult->Username;
 	echo $userFindResult->Password;
 }
-
 catch(PDOException $e){
 	echo $userFind . "<br>" . $e->getMessage();
 }
 ?>
-
-
 <html>
 <body>
-
 </body>
 </html>
-
 <?php
 $conn = null;
 ?>
