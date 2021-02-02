87ed4aeb826ad5d0adaf9fdff6ec3218dd39d11f
AstroFlyer <43142147+AstroFlyer@users.noreply.github.com>
2021-02-02 13:12:46 +0000

    New version of studycreation
    added checkboxes, need to re-visit

--- Software_Code/Liam/studycreation.php
+++ Software_Code/Liam/studycreation.php
@@ -8,18 +8,25 @@ include"config.php";
 <html>
 <body>
 <form action="questioncreation.php" method="post">
-  
+
   <label for="studycreator">Study Creator ID:</label><br>
   <input type="text" id="studycreator" name="studycreator" value="11"><br>
-  
+
   <label for="studyname">Study Name:</label><br>
   <input type="text" id="studyname" name="studyname"><br>
-  
+
   <label for="questionquantity">Number of Questions:</label><br>
-  <input type="text" id="questionquantity" name="questionquantity" value="3"><br>
-  
+  <input type="text" id="questionquantity" name="questionquantity" value="3"><br><br>
+
+  <label for="ethicssubmitted">Ethics Sheet Submitted: <label><br>
+  <input type="checkbox" id="ethicssubmitted" name="ethicssubmitted" value="Submitted" required><br>
+
+  <label for="ethicsapproval">Ethics Sheet Approved: <label><br>
+  <input type="checkbox" id="ethicsapproval" name="ethicsapproval" value="Approved" required><br><br>
+  <!--made approval disabled and use db to approve to checked state?-->
+
   <input type="submit" value="Submit">
-</form> 
+</form>
 </body>
 </html>
 
