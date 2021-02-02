c2a1e7f73489f23cf2431b511507d459e46a745d
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-02 13:50:12 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_questioncreation.php
+++ Software_Code/Sprint 2/liam_questioncreation.php
@@ -2,72 +2,71 @@
 session_start();
 include"config.php";
 
-	try{
-		$studyIDFind = $conn->prepare("SELECT * FROM Study ORDER BY ID DESC LIMIT 1");
-		$studyIDFind->execute();
-		$studyIDFindResult = $studyIDFind->fetch(PDO::FETCH_OBJ);
-		$studyID = $studyIDFindResult->ID;
+try{
+	$studyIDFind = $conn->prepare("SELECT * FROM Study ORDER BY ID DESC LIMIT 1");
+	$studyIDFind->execute();
+	$studyIDFindResult = $studyIDFind->fetch(PDO::FETCH_OBJ);
+	$studyID = $studyIDFindResult->ID;
 
-	}
-	catch(PDOException $e){
-		echo "Error: " . $e->getMessage();
-	}	
 }
-	?>
-	<html>
-	<body>
-		<?php
-		if($_SESSION["questionsremaining"]>0){
-			?>
-		<form action method="post" onsubmit="addQuestion()">
-			<label for="questiontext">Question Text:</label><br>
-			<input type="text" id="questiontext" name="questiontext" value="Is this an example question?"><br>
-			<label for="questiontype">Question Type:</label><br>
-			
-			
-			<!--
-			<select id="questiontype" name="questiontype">
-				<option value="1">Text</option>
-				<option value="2">Single Choice</option>
-				<option value="3">Multiple Choice</option>
-			</select>
-			<label for="questionanswerquantity">Question Answer Quantity:</label><br>
-			<input type="text" id="questionanswerquantity" name="questionanswerquantity" value="1"><br>
-			-->
-			
-			<input type="submit" value="Submit">
-		</form> 
-		<?php
-		$_SESSION["questionsremaining"]--;
-		}
-		else{
-			unset($_SESSION["questionsremaining"]);
-			echo "Questions submitted!";
-			?>
-			<a href="https://agilegroup05webapp.herokuapp.com/Software_Code/Sprint%202/liam_survey.php?surveyid=<?php echo $_SESSION['studyID']; ?>">Survey Link</a>
-			<?php
-		}
+catch(PDOException $e){
+	echo "Error: " . $e->getMessage();
+}	
+?>
+<html>
+<body>
+	<?php
+	if($_SESSION["questionsremaining"]>0){
 		?>
-	</body>
-	</html>
-
-	<script>
-	function addQuestion(){
-		<?php
-		$questiontext = $_POST["questiontext"];
-		try{
-			$questionInsert = "INSERT INTO question (QuestionText, QuestionAnswerCount, StudyID)
-			VALUES ('$questiontext', '1', '$studyID')";
-			$conn->exec($questionInsert);
-		}
+	<form action method="post" onsubmit="addQuestion()">
+		<label for="questiontext">Question Text:</label><br>
+		<input type="text" id="questiontext" name="questiontext" value="Is this an example question?"><br>
+		<label for="questiontype">Question Type:</label><br>
 		
-		catch(PDOException $e){
-			echo $questionInsert . "<br>" . $e->getMessage();
-		}
-		?>
-		return true;
+		
+		<!--
+		<select id="questiontype" name="questiontype">
+			<option value="1">Text</option>
+			<option value="2">Single Choice</option>
+			<option value="3">Multiple Choice</option>
+		</select>
+		<label for="questionanswerquantity">Question Answer Quantity:</label><br>
+		<input type="text" id="questionanswerquantity" name="questionanswerquantity" value="1"><br>
+		-->
+		
+		<input type="submit" value="Submit">
+	</form> 
+	<?php
+	$_SESSION["questionsremaining"]--;
 	}
-	</script>
+	else{
+		unset($_SESSION["questionsremaining"]);
+		echo "Questions submitted!";
+		?>
+		<a href="https://agilegroup05webapp.herokuapp.com/Software_Code/Sprint%202/liam_survey.php?surveyid=<?php echo $_SESSION['studyID']; ?>">Survey Link</a>
+		<?php
+	}
+	?>
+</body>
+</html>
+
+<script>
+function addQuestion(){
+	<?php
+	$questiontext = $_POST["questiontext"];
+	try{
+		$questionInsert = "INSERT INTO question (QuestionText, QuestionAnswerCount, StudyID)
+		VALUES ('$questiontext', '1', '$studyID')";
+		$conn->exec($questionInsert);
+	}
+	
+	catch(PDOException $e){
+		echo $questionInsert . "<br>" . $e->getMessage();
+	}
+	?>
+	return true;
+}
+</script>
 <?php
 $conn = null;
 ?>
