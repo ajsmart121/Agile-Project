ea3fd5b04749fc07b53b69b1a6418051f3f2da3f
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-02 16:48:24 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_questioncreation.php
+++ Software_Code/Sprint 2/liam_questioncreation.php
@@ -11,17 +11,20 @@ include"config.php";
 		$_SESSION["questionsremaining"] = $_GET['questionquantity'];
 	}
 	
+	echo nl2br("Questions Remaining: ".$_SESSION["questionsremaining"]."\r\n");
+	
+	$questiontext = $_POST["questiontext"];
+	
 	if(!isset($_SESSION["counter"])){
-		$_SESSION["counter"]=0;
+		$_SESSION["counter"] = $_GET['counter'];
 	}
-		
-	if($_SESSION["counter"] < $_SESSION["questionsremaining"]){
+	$_SESSION["counter"]++;
+	if($counter>1){
 		try{
-		$questiontext = $_POST["questiontext"];
-		$questionInsert = "INSERT INTO question (QuestionText, QuestionAnswerCount, StudyID)
-		VALUES ('$questiontext', '1', '$studyID')";
-		$conn->exec($questionInsert);
-		$_SESSION["counter"]++;
+			$questionInsert = "INSERT INTO question (QuestionText, QuestionAnswerCount, StudyID)
+			VALUES ('$questiontext', '1', '$studyID')";
+			$conn->exec($questionInsert);
+			$_SESSION["questionsremaining"]--;
 		}
 		
 		catch(PDOException $e){
@@ -30,17 +33,18 @@ include"config.php";
 	}
 	
 	if($_SESSION["questionsremaining"]>0){
-		
-	?>
-	<form action method="post">
-		<label for="questiontext">Question Text:</label><br>
-		<input type="text" id="questiontext" name="questiontext" value="Is this an example question?"><br>
-		<input type="submit" value="Submit">
-	</form> 
-	<?php
+		?>
+		<form action method="post">
+			<label for="questiontext">Question Text:</label><br>
+			<input type="text" id="questiontext" name="questiontext" value="Is this an example question?"><br>
+			<input type="hidden" id="counter" name="counter" value="0"><br>
+			<input type="submit" value="Submit">
+		</form> 
+		<?php
 	}
 	else{
 		unset($_SESSION["questionsremaining"]);
+		unset($_SESSION["counter"]);
 		echo "Questions submitted!";
 		?>
 		<a href="https://agilegroup05webapp.herokuapp.com/Software_Code/Sprint%202/liam_survey.php?surveyid=<?php echo $studyID; ?>">Survey Link</a>
