5a3824964dbc5169caacc2e979a87b33fd3f0860
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-02 16:51:25 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_questioncreation.php
+++ Software_Code/Sprint 2/liam_questioncreation.php
@@ -11,32 +11,28 @@ include"config.php";
 		$_SESSION["questionsremaining"] = $_GET['questionquantity'];
 	}
 	
-	echo nl2br("Questions Remaining: ".$_SESSION["questionsremaining"]."\r\n");
+	echo nl2br("Questions Remaining: ".$_SESSION["questionsremaining"]."\r\n\r\n");
 	
 	$questiontext = $_POST["questiontext"];
 	
-	if(!isset($_SESSION["counter"])){
-		$_SESSION["counter"] = $_GET['counter'];
-	}
-	$_SESSION["counter"]++;
-	if($counter>1){
-		try{
-			$questionInsert = "INSERT INTO question (QuestionText, QuestionAnswerCount, StudyID)
-			VALUES ('$questiontext', '1', '$studyID')";
-			$conn->exec($questionInsert);
-			$_SESSION["questionsremaining"]--;
-		}
-		
-		catch(PDOException $e){
-			echo $questionInsert . "<br>" . $e->getMessage();
-		}
+	if($questiontext != ""){
+	try{
+		$questionInsert = "INSERT INTO question (QuestionText, QuestionAnswerCount, StudyID)
+		VALUES ('$questiontext', '1', '$studyID')";
+		$conn->exec($questionInsert);
+		$_SESSION["questionsremaining"]--;
 	}
 	
+	catch(PDOException $e){
+		echo $questionInsert . "<br>" . $e->getMessage();
+	}
+	}
+
 	if($_SESSION["questionsremaining"]>0){
 		?>
 		<form action method="post">
 			<label for="questiontext">Question Text:</label><br>
-			<input type="text" id="questiontext" name="questiontext" value="Is this an example question?"><br>
+			<input type="text" id="questiontext" name="questiontext" value=""><br>
 			<input type="hidden" id="counter" name="counter" value="0"><br>
 			<input type="submit" value="Submit">
 		</form> 
@@ -44,7 +40,6 @@ include"config.php";
 	}
 	else{
 		unset($_SESSION["questionsremaining"]);
-		unset($_SESSION["counter"]);
 		echo "Questions submitted!";
 		?>
 		<a href="https://agilegroup05webapp.herokuapp.com/Software_Code/Sprint%202/liam_survey.php?surveyid=<?php echo $studyID; ?>">Survey Link</a>
