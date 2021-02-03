e0677ceeb2bdcf329b086a9fb252a21ce06f4d4f
PaulCrockford <43142223+PaulCrockford@users.noreply.github.com>
2021-02-03 18:31:09 +0000

    Font colours and sizes, need to rearrange buttons

--- Software_Code/Sprint 2/PaulAccessibility.js
+++ Software_Code/Sprint 2/PaulAccessibility.js
@@ -1,3 +1,4 @@
+//Normal Colours
 function ChangeColorNormal()
     {
       document.body.style.backgroundColor = "#3e5bc7";
@@ -11,8 +12,18 @@ function ChangeColorNormal()
       document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#3e5bc7";
       document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#3e5bc7";
       document.getElementById("Accessibility").style.backgroundColor = "#3e5bc7";
+      document.getElementById("XXSmallFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("XSmallFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("SmallFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("MediumFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("LargeFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("XLargeFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("XXLargeFontSize").style.backgroundColor = "#3e5bc7";
+      document.getElementById("FontSize").style.backgroundColor = "#3e5bc7";
+
     }
 
+    //Protan Colours
     function ChangeColorProtan()
     {
       document.body.style.backgroundColor = "#0064b1";
@@ -26,8 +37,17 @@ function ChangeColorNormal()
       document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#0064b1";
       document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#0064b1";
       document.getElementById("Accessibility").style.backgroundColor = "#0064b1";
+      document.getElementById("XXSmallFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("XSmallFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("SmallFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("MediumFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("LargeFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("XLargeFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("XXLargeFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("FontSize").style.backgroundColor = "#0064b1";
     }
 
+    //Deutran Colours
     function ChangeColorDeutran()
     {
       document.body.style.backgroundColor = "#0064b1";
@@ -41,8 +61,17 @@ function ChangeColorNormal()
       document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#0064b1";
       document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#0064b1";
       document.getElementById("Accessibility").style.backgroundColor = "#0064b1";
+      document.getElementById("XXSmallFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("XSmallFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("SmallFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("MediumFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("LargeFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("XLargeFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("XXLargeFontSize").style.backgroundColor = "#0064b1";
+      document.getElementById("FontSize").style.backgroundColor = "#0064b1";
     }
 
+    //Tritan Colours
     function ChangeColorTritan()
     {
       document.body.style.backgroundColor = "#007385";
@@ -56,4 +85,159 @@ function ChangeColorNormal()
       document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#007385";
       document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#007385";
       document.getElementById("Accessibility").style.backgroundColor = "#007385";
+      document.getElementById("XXSmallFontSize").style.backgroundColor = "#007385";
+      document.getElementById("XSmallFontSize").style.backgroundColor = "#007385";
+      document.getElementById("SmallFontSize").style.backgroundColor = "#007385";
+      document.getElementById("MediumFontSize").style.backgroundColor = "#007385";
+      document.getElementById("LargeFontSize").style.backgroundColor = "#007385";
+      document.getElementById("XLargeFontSize").style.backgroundColor = "#007385";
+      document.getElementById("XXLargeFontSize").style.backgroundColor = "#007385";
+      document.getElementById("FontSize").style.backgroundColor = "#007385";
+    }
+
+    //Font size xx-small
+    function XXSmallFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "xx-small";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "xx-small";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "xx-small";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "xx-small";
+      document.getElementById("button2").style.fontSize = "xx-small";
+      document.getElementById("button5").style.fontSize = "xx-small";
+      document.getElementById("button3").style.fontSize = "xx-small";
+      document.getElementById("button6").style.fontSize = "xx-small";
+      document.getElementById("button4").style.fontSize = "xx-small";
+      document.getElementById("XXSmallFontSize").style.fontSize = "xx-small";
+      document.getElementById("XSmallFontSize").style.fontSize = "xx-small";
+      document.getElementById("SmallFontSize").style.fontSize = "xx-small";
+      document.getElementById("MediumFontSize").style.fontSize = "xx-small";
+      document.getElementById("LargeFontSize").style.fontSize = "xx-small";
+      document.getElementById("XLargeFontSize").style.fontSize = "xx-small";
+      document.getElementById("XXLargeFontSize").style.fontSize = "xx-small";
+    }
+
+    //Font size x-small
+    function XSmallFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "x-small";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "x-small";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "x-small";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "x-small";
+      document.getElementById("button2").style.fontSize = "x-small";
+      document.getElementById("button5").style.fontSize = "x-small";
+      document.getElementById("button3").style.fontSize = "x-small";
+      document.getElementById("button6").style.fontSize = "x-small";
+      document.getElementById("button4").style.fontSize = "x-small";
+      document.getElementById("XXSmallFontSize").style.fontSize = "x-small";
+      document.getElementById("XSmallFontSize").style.fontSize = "x-small";
+      document.getElementById("SmallFontSize").style.fontSize = "x-small";
+      document.getElementById("MediumFontSize").style.fontSize = "x-small";
+      document.getElementById("LargeFontSize").style.fontSize = "x-small";
+      document.getElementById("XLargeFontSize").style.fontSize = "x-small";
+      document.getElementById("XXLargeFontSize").style.fontSize = "x-small";
+    }
+
+    //Font size small
+    function SmallFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "small";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "small";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "small";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "small";
+      document.getElementById("button2").style.fontSize = "small";
+      document.getElementById("button5").style.fontSize = "small";
+      document.getElementById("button3").style.fontSize = "small";
+      document.getElementById("button6").style.fontSize = "small";
+      document.getElementById("button4").style.fontSize = "small";
+      document.getElementById("XXSmallFontSize").style.fontSize = "small";
+      document.getElementById("XSmallFontSize").style.fontSize = "small";
+      document.getElementById("SmallFontSize").style.fontSize = "small";
+      document.getElementById("MediumFontSize").style.fontSize = "small";
+      document.getElementById("LargeFontSize").style.fontSize = "small";
+      document.getElementById("XLargeFontSize").style.fontSize = "small";
+      document.getElementById("XXLargeFontSize").style.fontSize = "small";
+    }
+
+    //Font size medium
+    function MediumFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "medium";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "medium";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "medium";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "medium";
+      document.getElementById("button2").style.fontSize = "medium";
+      document.getElementById("button5").style.fontSize = "medium";
+      document.getElementById("button3").style.fontSize = "medium";
+      document.getElementById("button6").style.fontSize = "medium";
+      document.getElementById("button4").style.fontSize = "medium";
+      document.getElementById("XXSmallFontSize").style.fontSize = "medium";
+      document.getElementById("XSmallFontSize").style.fontSize = "medium";
+      document.getElementById("SmallFontSize").style.fontSize = "medium";
+      document.getElementById("MediumFontSize").style.fontSize = "medium";
+      document.getElementById("LargeFontSize").style.fontSize = "medium";
+      document.getElementById("XLargeFontSize").style.fontSize = "medium";
+      document.getElementById("XXLargeFontSize").style.fontSize = "medium";
+    }
+
+    //Font size large
+    function LargeFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "large";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "large";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "large";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "large";
+      document.getElementById("button2").style.fontSize = "large";
+      document.getElementById("button5").style.fontSize = "large";
+      document.getElementById("button3").style.fontSize = "large";
+      document.getElementById("button6").style.fontSize = "large";
+      document.getElementById("button4").style.fontSize = "large";
+      document.getElementById("XXSmallFontSize").style.fontSize = "large";
+      document.getElementById("XSmallFontSize").style.fontSize = "large";
+      document.getElementById("SmallFontSize").style.fontSize = "large";
+      document.getElementById("MediumFontSize").style.fontSize = "large";
+      document.getElementById("LargeFontSize").style.fontSize = "large";
+      document.getElementById("XLargeFontSize").style.fontSize = "large";
+      document.getElementById("XXLargeFontSize").style.fontSize = "large";
+    }
+
+    //Font size x-large
+    function XLargeFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "x-large";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "x-large";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "x-large";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "x-large";
+      document.getElementById("button2").style.fontSize = "x-large";
+      document.getElementById("button5").style.fontSize = "x-large";
+      document.getElementById("button3").style.fontSize = "x-large";
+      document.getElementById("button6").style.fontSize = "x-large";
+      document.getElementById("button4").style.fontSize = "x-large";
+      document.getElementById("XXSmallFontSize").style.fontSize = "x-large";
+      document.getElementById("XSmallFontSize").style.fontSize = "x-large";
+      document.getElementById("SmallFontSize").style.fontSize = "x-large";
+      document.getElementById("MediumFontSize").style.fontSize = "x-large";
+      document.getElementById("LargeFontSize").style.fontSize = "x-large";
+      document.getElementById("XLargeFontSize").style.fontSize = "x-large";
+      document.getElementById("XXLargeFontSize").style.fontSize = "x-large";
+    }
+
+    //Font size xx-large
+    function XXLargeFontSize()
+    {
+      document.getElementById("AccessibilityButtonNormal").style.fontSize = "xx-large";
+      document.getElementById("AccessibilityButtonProtan").style.fontSize = "xx-large";
+      document.getElementById("AccessibilityButtonDeutran").style.fontSize = "xx-large";
+      document.getElementById("AccessibilityButtonTritan").style.fontSize = "xx-large";
+      document.getElementById("button2").style.fontSize = "xx-large";
+      document.getElementById("button5").style.fontSize = "xx-large";
+      document.getElementById("button3").style.fontSize = "xx-large";
+      document.getElementById("button6").style.fontSize = "xx-large";
+      document.getElementById("button4").style.fontSize = "xx-large";
+      document.getElementById("XXSmallFontSize").style.fontSize = "xx-large";
+      document.getElementById("XSmallFontSize").style.fontSize = "xx-large";
+      document.getElementById("SmallFontSize").style.fontSize = "xx-large";
+      document.getElementById("MediumFontSize").style.fontSize = "xx-large";
+      document.getElementById("LargeFontSize").style.fontSize = "xx-large";
+      document.getElementById("XLargeFontSize").style.fontSize = "xx-large";
+      document.getElementById("XXLargeFontSize").style.fontSize = "xx-large";
     }
