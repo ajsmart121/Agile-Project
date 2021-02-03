f1a84d7a704c58409277eebb7a14f4733daf615e
PaulCrockford <43142223+PaulCrockford@users.noreply.github.com>
2021-02-03 15:07:45 +0000

    Working colour blind options

---
+++ Software_Code/Sprint 2/PaulAccessibility.js
@@ -0,0 +1,59 @@
+function ChangeColorNormal()
+    {
+      document.body.style.backgroundColor = "#3e5bc7";
+      document.getElementById("button2").style.backgroundColor = "#3e5bc7";
+      document.getElementById("button5").style.backgroundColor = "#c42986";
+      document.getElementById("button3").style.backgroundColor = "#3e5bc7";
+      document.getElementById("button6").style.backgroundColor = "#3e5bc7";
+      document.getElementById("box1").style.borderColor = "#800080";
+      document.getElementById("AccessibilityButtonNormal").style.backgroundColor = "#3e5bc7";
+      document.getElementById("AccessibilityButtonProtan").style.backgroundColor = "#3e5bc7";
+      document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#3e5bc7";
+      document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#3e5bc7";
+      document.getElementById("Accessibility").style.backgroundColor = "#3e5bc7";
+    }
+
+    function ChangeColorProtan()
+    {
+      document.body.style.backgroundColor = "#0064b1";
+      document.getElementById("button2").style.backgroundColor = "#0064b1";
+      document.getElementById("button5").style.backgroundColor = "#004186";
+      document.getElementById("button3").style.backgroundColor = "#0064b1";
+      document.getElementById("button6").style.backgroundColor = "#0064b1";
+      document.getElementById("box1").style.borderColor = "#25486e";
+      document.getElementById("AccessibilityButtonNormal").style.backgroundColor = "#0064b1";
+      document.getElementById("AccessibilityButtonProtan").style.backgroundColor = "#0064b1";
+      document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#0064b1";
+      document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#0064b1";
+      document.getElementById("Accessibility").style.backgroundColor = "#0064b1";
+    }
+
+    function ChangeColorDeutran()
+    {
+      document.body.style.backgroundColor = "#0064b1";
+      document.getElementById("button2").style.backgroundColor = "#0064b1";
+      document.getElementById("button5").style.backgroundColor = "#555e6c";
+      document.getElementById("button3").style.backgroundColor = "#0064b1";
+      document.getElementById("button6").style.backgroundColor = "#0064b1";
+      document.getElementById("box1").style.borderColor = "#235485";
+      document.getElementById("AccessibilityButtonNormal").style.backgroundColor = "#0064b1";
+      document.getElementById("AccessibilityButtonProtan").style.backgroundColor = "#0064b1";
+      document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#0064b1";
+      document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#0064b1";
+      document.getElementById("Accessibility").style.backgroundColor = "#0064b1";
+    }
+
+    function ChangeColorTritan()
+    {
+      document.body.style.backgroundColor = "#007385";
+      document.getElementById("button2").style.backgroundColor = "#007385";
+      document.getElementById("button5").style.backgroundColor = "#ab3c4b";
+      document.getElementById("button3").style.backgroundColor = "#007385";
+      document.getElementById("button6").style.backgroundColor = "#007385";
+      document.getElementById("box1").style.borderColor = "#7a474c";
+      document.getElementById("AccessibilityButtonNormal").style.backgroundColor = "#007385";
+      document.getElementById("AccessibilityButtonProtan").style.backgroundColor = "#007385";
+      document.getElementById("AccessibilityButtonDeutran").style.backgroundColor = "#007385";
+      document.getElementById("AccessibilityButtonTritan").style.backgroundColor = "#007385";
+      document.getElementById("Accessibility").style.backgroundColor = "#007385";
+    }
