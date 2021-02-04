e34dbfb193ef57165918512fbfa0a88ed07aa53c
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-04 16:05:31 +0000

    Update liam_survey.php

--- Software_Code/Sprint 2/liam_survey.php
+++ Software_Code/Sprint 2/liam_survey.php
@@ -21,7 +21,8 @@ catch(PDOException $e){
 
 ?>
 <html>
-<link rel="stylesheet" type="text/css" href="fergus_login_fixed_font.css"> <!--use the theme CSS-->
+	<link rel="stylesheet" type="text/css" href="fergus_login_fixed_font.css"> <!--use the theme CSS-->
+	<link rel="stylesheet" type="text/css" href="PaulTheme2.css"> <!--use the theme CSS-->
 <body>
 
 <form action="liam_submitsurvey.php" method="post">
