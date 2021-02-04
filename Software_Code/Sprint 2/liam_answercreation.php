210cc64d4faf67a2ffdf0ebdaeb2b8333ef55783
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-04 14:14:35 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_answercreation.php
+++ Software_Code/Sprint 2/liam_answercreation.php
@@ -23,14 +23,14 @@ echo nl2br($studyID."\r\n");
 
 <form action="liam_answerscreated.php method="post">
 	<?php
-	/*
+	
 	for($i = 0; $i < $questionanswerquantity; $i++){
 		?>
 		<label for="option[<?php $i+1 ?>]"> <?php echo "Option ".$i+1; ?> </label><br>
 		<input type="text" id="option[<?php $i+1 ?>]" name="option[<?php $i+1 ?>]" value=""><br>
 		<?php
 	}
-	*/
+	
 	?>
 	<input type="submit" value="Submit">
 </form> 
