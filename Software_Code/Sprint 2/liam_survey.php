d3f16db3a871810e71ba550bb11db4371cfdc4f8
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-04 22:58:50 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_survey.php
+++ Software_Code/Sprint 2/liam_survey.php
@@ -8,7 +8,8 @@ include"config.php";
 $studyID = $_GET['studyID'];
 
 try{
-	$QuestionsFind = $conn->prepare("SELECT QuestionText, ID, StudyID FROM Question
+	$QuestionsFind = $conn->prepare("SELECT QuestionText, ID, StudyID, QuestionAnswerCount, questiontype 
+	FROM Question
 	WHERE StudyID = '$studyID'");
 	$QuestionsFind->execute();
 	$QuestionsFindResult = $QuestionsFind->fetchALL();
@@ -21,8 +22,6 @@ catch(PDOException $e){
 
 ?>
 <html>
-	<link rel="stylesheet" type="text/css" href="fergus_login_fixed_font.css"> <!--use the theme CSS-->
-	<link rel="stylesheet" type="text/css" href="PaulTheme2.css"> <!--use the theme CSS-->
 <body>
 
 <form action="liam_submitsurvey.php" method="post">
@@ -31,8 +30,15 @@ catch(PDOException $e){
 	for($i = 0; $i < $questionCount; $i++){
 		echo "ID: ".$QuestionsFindResult[$i][1];
 		?>
+		<!-- The line below prints out the QuestionText -->
 		<label for="answer[<?php $i+1 ?>]"> <?php echo $QuestionsFindResult[$i][0]; ?> </label><br>
-		<input type="text" id="answer[<?php $i+1 ?>]" name="answer[<?php $i+1 ?>]" value=""><br>
+		<?php
+		//The line below checks the question type
+		if($QuestionsFindResult[$i][4]!="textbox"){
+			echo "This is not a textbox, this is a ".$QuestionsFindResult[$i][4];
+		}	
+		?>
+		<br><input type="text" id="answer[<?php $i+1 ?>]" name="answer[<?php $i+1 ?>]" value=""><br>
 	<?php
 	}
 	
