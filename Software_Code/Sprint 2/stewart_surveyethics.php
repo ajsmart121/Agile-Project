d9a7569aafc8d548e67acf3a4050274e558e87f3
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-05 13:22:21 +0000

    Update stewart_surveyethics.php

--- Software_Code/Sprint 2/stewart_surveyethics.php
+++ Software_Code/Sprint 2/stewart_surveyethics.php
@@ -21,7 +21,7 @@ catch(PDOException $e){
   <link rel="stylesheet" type="text/css" href="PaulTheme2.css"> <!--use the theme CSS-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"><!--use the bootstrap-->
     <title>
-        Login Page
+        Survey Ethics Check
     </title>
 <!--setbackground to Dundee Colours-->
     <style>
