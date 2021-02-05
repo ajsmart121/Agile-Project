08176da32eba736107ed9d9849819adb7c0bb0f1
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-05 09:55:13 +0000

    Update aj_userlevellogin.php

--- Software_Code/Sprint 2/aj_userlevellogin.php
+++ Software_Code/Sprint 2/aj_userlevellogin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
   <link rel="stylesheet" type="text/css" href="fergus_login_fixed_font.css"> <!--use the theme CSS-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <?php
 session_start();
 include"config.php";
