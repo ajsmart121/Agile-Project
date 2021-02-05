bd06e81e34e672c0f2325051ccfa0c8257c059ac
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-05 09:54:03 +0000

    Update aj_userlevellogin.php

--- Software_Code/Sprint 2/aj_userlevellogin.php
+++ Software_Code/Sprint 2/aj_userlevellogin.php
@@ -9,15 +9,18 @@ $conn = null;
 
 <html>
 <body>
-<form action="aj_userleveltest.php" method="post">
-  <label for="study">Study ID:</label><br>
-  <input type="text" id="study" name="study" value="1"><br>
-  <label for="username">Username:</label><br>
-  <input type="text" id="username" name="username" value="Username"><br>
-  <label for="password">Password:</label><br>
-  <input type="text" id="password" name="password" value="Password"><br><br>
-  <input type="submit" value="Submit">
-</form>
+  
+  <div class="container">
+    <form action="aj_userleveltest.php" method="post">
+      <label for="study">Study ID:</label><br>
+      <input type="text" id="study" name="study" value="1"><br>
+      <label for="username">Username:</label><br>
+      <input type="text" id="username" name="username" value="Username"><br>
+      <label for="password">Password:</label><br>
+      <input type="text" id="password" name="password" value="Password"><br><br>
+      <input type="submit" value="Submit">
+    </form>
+  </div>
 </body>
 </html>
 
