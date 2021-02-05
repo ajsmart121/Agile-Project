a1bded56747a0d3ede7600420416c614c2764309
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-05 00:34:08 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_survey.php
+++ Software_Code/Sprint 2/liam_survey.php
@@ -51,8 +51,8 @@ catch(PDOException $e){
 			//We then start a for loop that runs for the amount of answers assigned to the question
 			for($j = 0; $j < $QuestionsFindResult[$i][3]; $j++){
 				?>
-				<input type="radio" id="answer[<?php $j+1 ?>]" name="answer[<?php $i+1 ?>]" value="<?php echo $AnswersFindResult[$j][1] ?>">
-				<label for="answer[<?php $j+1 ?>]"><?php echo $AnswersFindResult[$j][1] ?></label><br>
+				<input type="radio" id="answer[<?php echo $j+1; ?>]" name="answer[<?php echo $i+1; ?>]" value="<?php echo $AnswersFindResult[$j][1] ?>">
+				<label for="answer[<?php echo $i+1; ?>]"><?php echo $AnswersFindResult[$j][1] ?></label><br>
 				<?php
 			}	
 		}
