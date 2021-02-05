1d872a2be03879a6080528ddc1e25c890f0e26d0
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-05 12:40:44 +0000

    Add files via upload

--- Software_Code/Sprint 2/stewart_surveyethics.php
+++ Software_Code/Sprint 2/stewart_surveyethics.php
@@ -3,9 +3,7 @@
 session_start();
 include"config.php";
 $userID = $_SESSION['user'];
-?>
 
-<?php
 $studyID = $_GET['studyID'];
 
 try{
@@ -19,10 +17,62 @@ catch(PDOException $e){
 }
 
 ?>
-<html>
-<body>
+<head>
+  <link rel="stylesheet" type="text/css" href="PaulTheme2.css"> <!--use the theme CSS-->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"><!--use the bootstrap-->
+    <title>
+        Login Page
+    </title>
+<!--setbackground to Dundee Colours-->
+    <style>
+        body {
+          background-color: #3e5bc7;
+        }
+    </style>
+</head>
 
-<form action="liam_survey.php" method="get">
+<!--Dundee Logo -->
+<body>
+  <div class="dropdown" id="dropdownAccessibility">
+    <button onclick="showAccessibilty()" class="dropbtn">Colours</button>
+  <div class = "Accessibility" id="Accessibility">
+      <button herf="#" id="AccessibilityButtonNormal" class="AccessibilityButton" onclick="ChangeColorNormal()" type = "NormalColour">Normal</button>
+      <button herf="#" id="AccessibilityButtonProtan" class="AccessibilityButton" onclick="ChangeColorProtan()" type = "ProtanColour">Protan</button>
+      <button herf="#" id="AccessibilityButtonDeutran" class="AccessibilityButton" onclick="ChangeColorDeutran()" type = "DeutranColour">Deutran</button>
+      <button herf="#" id="AccessibilityButtonTritan" class="AccessibilityButton" onclick="ChangeColorTritan()" type = "TritanColour">Tritan</button>
+  </div>
+</div>
+
+ <div class="dropdown" id="dropdownFontSize">
+   <button onclick="showFont()" class="dropbtn">Font Size</button>
+  <div class = "FontSize" id="FontSize">
+    <button herf="#" id="XXSmallFontSize" class="FontButton" onclick="XXSmallFontSize()" type = "XXSmallTextSize">XX-Small Font Size</button>
+    <button herf="#" id="XSmallFontSize" class="FontButton" onclick="XSmallFontSize()" type = "XSmallTextSize">X-Small Font Size</button>
+    <button herf="#" id="SmallFontSize" class="FontButton" onclick="SmallFontSize()" type = "SmallTextSize">Small Font Size</button>
+    <button herf="#" id="MediumFontSize" class="FontButton" onclick="MediumFontSize()" type = "MediumFontSize">Medium Font Size</button>
+    <button herf="#" id="LargeFontSize" class="FontButton" onclick="LargeFontSize()" type = "LargeFontSize">Large Font Size</button>
+    <button herf="#" id="XLargeFontSize" class="FontButton" onclick="XLargeFontSize()" type = "XLargeFontSize">X-Large Font Size</button>
+    <button herf="#" id="XXLargeFontSize" class="FontButton" onclick="XXLargeFontSize()" type = "XXLargeFontSize">XX-Large Font Size</button>
+  </div>
+</div>
+
+<script src="PaulAccessibility.js"></script>
+
+<div class = "Header Layout">
+  <img src="PaulWhiteLogo.png" alt="Logo" style="width:20%">
+</div>
+
+<!--Adds home button-->
+<div class = "Header Layout">
+  <input id="button2" type="button2" class="button2" value="Home" onclick="document.location='PaulHome.php'">
+
+  <!--For accessibility options-->
+
+</div>
+
+<!--creates main page container-->
+<div class = "Body Layout">
+	<form action="liam_survey.php" method="get">
 	<?php
 	echo nl2br($StudyFindResult->StudyName."\r\n");
 	echo nl2br("If you have any questions, please email ".$StudyFindResult->CreatorEmail."\r\n");
@@ -35,12 +85,10 @@ catch(PDOException $e){
 	<input type="checkbox" name="Understood" required><br><br>
 
 	<input type="submit" value="Submit">
-</form> 
+	</form> 
+</div>
 
 </body>
-</html>
-
-
 <?php
 $conn = null;
 ?>
