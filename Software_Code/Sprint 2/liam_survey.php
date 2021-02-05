744b65660601f9867accf88529fad4ef03d20e8e
lsm909303 <57756762+lsm909303@users.noreply.github.com>
2021-02-05 10:36:09 +0000

    Add files via upload

--- Software_Code/Sprint 2/liam_survey.php
+++ Software_Code/Sprint 2/liam_survey.php
@@ -20,29 +20,11 @@ catch(PDOException $e){
 	echo "Error: " . $e->getMessage();
 }
 
-try{
-	$StudyFind = $conn->prepare("SELECT StudyName, CreatorEmail, EthicsLink, EthicsDisclosureText FROM Study
-    WHERE ID = '$studyID'");
-    $StudyFind->execute();
-    $StudyFindResult = $StudyFind->fetch(PDO::FETCH_OBJ);
-}
-catch(PDOException $e){
-	echo "Error: " . $e->getMessage();
-}
-
 ?>
 <html>
 <body>
 
 <form action="liam_submitsurvey.php" method="post">
-	<?php
-	echo nl2br($StudyFindResult->StudyName."\r\n");
-	echo nl2br("If you have any questions, please email ".$StudyFindResult->CreatorEmail."\r\n");
-    echo nl2br("Ethical assessment and statement: ".$StudyFindResult->EthicsLink."\r\n");
-    echo nl2br("Ethics Disclosure: ".$StudyFindResult->EthicsDisclosureText."\r\n");
-    echo nl2br("\r\n I understand and agree to the above Ethics Disclosure. \r\n I am aware of my rights and how to contact should a question arise.\r\n");
-    ?>
-	<input type="checkbox" name="Understood" required><br><br>
 	<?php
 	for($i = 0; $i < $questionCount; $i++){
 		?>
