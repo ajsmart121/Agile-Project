4b32fd730458bf60b555c3703e9182a53ceb3394
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-05 12:56:51 +0000

    Update aj_userlevellogin.php

--- Software_Code/Sprint 2/aj_userlevellogin.php
+++ Software_Code/Sprint 2/aj_userlevellogin.php
@@ -11,6 +11,45 @@ unset($_SESSION['user']);
 
 <html>
 <body>
+  <!--Dundee Logo -->
+<body>
+  <div class="dropdown" id="dropdownAccessibility">
+    <button onclick="showAccessibilty()" class="dropbtn">Colours</button>
+  <div class = "Accessibility" id="Accessibility">
+      <button herf="#" id="AccessibilityButtonNormal" class="AccessibilityButton" onclick="ChangeColorNormal()" type = "NormalColour">Normal</button>
+      <button herf="#" id="AccessibilityButtonProtan" class="AccessibilityButton" onclick="ChangeColorProtan()" type = "ProtanColour">Protan</button>
+      <button herf="#" id="AccessibilityButtonDeutran" class="AccessibilityButton" onclick="ChangeColorDeutran()" type = "DeutranColour">Deutran</button>
+      <button herf="#" id="AccessibilityButtonTritan" class="AccessibilityButton" onclick="ChangeColorTritan()" type = "TritanColour">Tritan</button>
+  </div>
+</div>
+
+ <div class="dropdown" id="dropdownFontSize">
+   <button onclick="showFont()" class="dropbtn">Font Size</button>
+  <div class = "FontSize" id="FontSize">
+    <button herf="#" id="XXSmallFontSize" class="FontButton" onclick="XXSmallFontSize()" type = "XXSmallTextSize">XX-Small Font Size</button>
+    <button herf="#" id="XSmallFontSize" class="FontButton" onclick="XSmallFontSize()" type = "XSmallTextSize">X-Small Font Size</button>
+    <button herf="#" id="SmallFontSize" class="FontButton" onclick="SmallFontSize()" type = "SmallTextSize">Small Font Size</button>
+    <button herf="#" id="MediumFontSize" class="FontButton" onclick="MediumFontSize()" type = "MediumFontSize">Medium Font Size</button>
+    <button herf="#" id="LargeFontSize" class="FontButton" onclick="LargeFontSize()" type = "LargeFontSize">Large Font Size</button>
+    <button herf="#" id="XLargeFontSize" class="FontButton" onclick="XLargeFontSize()" type = "XLargeFontSize">X-Large Font Size</button>
+    <button herf="#" id="XXLargeFontSize" class="FontButton" onclick="XXLargeFontSize()" type = "XXLargeFontSize">XX-Large Font Size</button>
+  </div>
+</div>
+
+<script src="PaulAccessibility.js"></script>
+
+<div class = "Header Layout">
+  <img src="PaulWhiteLogo.png" alt="Logo" style="width:20%">
+</div>
+
+<!--Adds home button-->
+<div class = "Header Layout">
+  <input id="button2" type="button2" class="button2" value="Home" onclick="document.location=''">
+
+  <!--For accessibility options-->
+
+</div>
+  
   
   <div class="container">
     <form action="aj_userleveltest.php" method="post">
