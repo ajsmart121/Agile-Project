bc6465cd6d7c1cee1a01e3f017db626a34757cdd
AJ Smart <43140891+ajsmart121@users.noreply.github.com>
2021-02-05 12:55:20 +0000

    Update aj_userlevellogin.php

--- Software_Code/Sprint 2/aj_userlevellogin.php
+++ Software_Code/Sprint 2/aj_userlevellogin.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
   <link rel="stylesheet" type="text/css" href="fergus_login_fixed_font.css"> <!--use the theme CSS-->
+  <link rel="stylesheet" type="text/css" href"PaulTheme2.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 <?php
 session_start();
@@ -13,8 +14,6 @@ unset($_SESSION['user']);
   
   <div class="container">
     <form action="aj_userleveltest.php" method="post">
-      <label for="study">Study ID:</label><br>
-      <input type="text" id="study" name="study" value="1"><br>
       <label for="username">Username:</label><br>
       <input type="text" id="username" name="username" value="username"><br>
       <label for="password">Password:</label><br>
